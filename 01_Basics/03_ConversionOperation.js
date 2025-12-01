31e3e2294704c6b999ac2069b17b853c0d80625e
Surendra Meshram <surend2000@gmail.com>
2025-12-01 17:11:59 +0000

    Conversion of DataTypes

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,28 @@
+let score ="surendra"
+//console.log(typeof score);
+//console.log(typeof (score)); 2nd way to write
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//"33"=> 33
+//"33abc"=> NaN after connverting its  type will be number but value will be NaN
+// true=> 1; false=> 0
+//converting null=> number results 0
+//unndefine=> undefined
+
+let isLoggedIn=0
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+// 1=> true; 0=> false
+//""=> false
+//"surend"=>true
+
+ let someNumber=33
+ let stringNumber=String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof(stringNumber));
+ 
+ 
